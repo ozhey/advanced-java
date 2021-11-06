80ba1293993cbb2544d99bd910c06ab045eb59d2
Oz <ozhey119@gmail.com>
2021-11-06 21:42:36 +0200

    init mmn14q2

---
+++ Mmn14Q2/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("AddressBook.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("AddressBook");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
