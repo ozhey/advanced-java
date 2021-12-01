81ad8adf824495d5f22312bad3368519dce79379
Oz <ozhey119@gmail.com>
2021-12-01 14:18:50 +0200

    improve code readability

--- Mmn13Q1/QuestionRepo.java
+++ Mmn13Q1/QuestionRepo.java
@@ -6,44 +6,34 @@ import java.util.Scanner;
 // QuestionRepo reads a file and constructs a list of questions.
 public class QuestionRepo {
 
-    private static final int NUM_OF_LINES_PER_Q = 5; // every question consists of 5 lines - 1 question and 4 answers
-    private Question[] questions;
+    private static final int NUM_OF_ANSWERS = 4; // every question consists of 5 lines - 1 question and 4 answers
+    private ArrayList<Question> questions;
 
     // constructor. reads the file into the questions and answers ArrayLists.
     public QuestionRepo(String fileName) {
-        int counter = 0;
-        ArrayList<String> questionsList = new ArrayList<String>();
-        ArrayList<ArrayList<String>> answersList = new ArrayList<ArrayList<String>>();
+        String currentQuestion = "";
+        questions = new ArrayList<>();
+        ArrayList<String> answersList = new ArrayList<>();
         try (Scanner input = new Scanner(new File(fileName + ".txt"))){
             while (input.hasNext()) {
-                String st = input.nextLine();
-                if (counter % NUM_OF_LINES_PER_Q == 0) { // If the line number divides with no remainder, it's a question
-                    questionsList.add(st);
-                } else {
-                    if (counter % NUM_OF_LINES_PER_Q == 1) { // If the line number divides with a remainder of 1, it's the first answer for the question
-                        answersList.add(new ArrayList<String>());
-                    }
-                    answersList.get(counter / NUM_OF_LINES_PER_Q).add(st); // Add the answer to the corresponding question
+                currentQuestion = input.nextLine();;
+                for (int i = 0; i < NUM_OF_ANSWERS; i++) {
+                    answersList.add(input.nextLine()); 
                 }
-                counter++;
+                this.questions.add(new Question(currentQuestion, answersList));
+                answersList = new ArrayList<>();
             }
-        } catch (FileNotFoundException e) {
-            this.questions = new Question[0];
-        }
-        this.questions = new Question[questionsList.size()];
-        for (int i = 0; i < this.questions.length; i++) {
-            this.questions[i] = new Question(questionsList.get(i), answersList.get(i));
-        }
+        } catch (FileNotFoundException e) { }
     }
 
     // returns question number questionNum
     public Question getQuestion(int questionNum) {
-        return questions[questionNum];
+        return questions.get(questionNum);
     }
 
     // returns the total number of questions
     public int getNumOfQuestions() {
-        return questions.length;
+        return questions.size();
     }
 
 
