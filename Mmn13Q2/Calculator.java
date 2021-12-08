9cd161848157f36dbfea58350cb63b276f2ac86a
Oz <ozhey119@gmail.com>
2021-12-08 22:20:13 +0200

    split controller into logic and controller

--- Mmn13Q2/Calculator.java
+++ Mmn13Q2/Calculator.java
@@ -1,25 +1,123 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+// this class implements the required logic for the calculator application
+// the calculator's logic is based on Apple's macOS basic calculator and follows the rules of the assignment.
+// it computes the result on every operation, so it doesn't help with operator precedence.
+public class Calculator {
 
+    private String operator;
+    private double prevNum, curNum;
+    private boolean opFlag; // will be true if the last action was an operation
+    private String text;
 
-public class Calculator extends Application {
-	
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Calculator.fxml"));
-        Scene scene = new Scene(root);
-        stage.setTitle("Calculator");
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
+    // constructor
+    public Calculator() {
+        text = "";
+        operator = "";
+        prevNum = 0;
+        curNum = 0;
+        opFlag = false;
     }
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
 
+    // returns the current result so it can be displayed on screen
+    public String getCurrentResult() {
+        return text;
+    }
 
-}	
+    // resets the calculator's memory
+    public void reset() {
+        text = "";
+        prevNum = 0;
+        curNum = 0;
+        operator = "";
+        opFlag = false;
+    }
+
+    // changes the sign of the current number.
+    public void changeSign() {
+        double number = parseDouble(text);
+        text = fixedNumber(-number);
+    }
+
+    // calculates the result
+    public void equal() {
+        curNum = parseDouble(text);
+        text = calcResult();
+        prevNum = parseDouble(text);
+        operator = "";
+        opFlag = false;
+    }
+
+    // adds a comma to the current text unless there's already a comma
+    public void comma() {
+        String curText = text;
+        if (opFlag || curText.length() == 0) {
+            text = "0.";
+        } else if (!curText.contains(".")) {
+            text = curText + ".";
+        }
+        opFlag = false;
+    }
+
+    // gets a string representing an operation (+, -, * or /). It
+    // calculates the last result, and only then it changes the current operator.
+    public void operation(String op) {
+        if (opFlag) {
+            operator = op;
+        } else {
+            opFlag = true;
+            curNum = parseDouble(text);
+            text = calcResult();
+            prevNum = parseDouble(text);
+            operator = op;
+        }
+    }
+
+    // gets a number and adds it to the currently displayed number
+    public void number(String num) {
+        if (opFlag) {
+            text = num;
+        } else {
+            text += num;
+        }
+        opFlag = false;
+    }
+
+    // parseDouble is a wrapper around Double.parseDouble, but it also checks if the
+    // parsed text isn't empty and returns 0 if the parsing fails.
+    private double parseDouble(String text) {
+        if (text != null && text.length() > 0) {
+            try {
+                return Double.parseDouble(text);
+            } catch (NumberFormatException e) {
+                // supposedly it's not possible to insert input that's not convertable to a
+                // double
+                return 0;
+            }
+        }
+        return 0;
+    }
+
+    // calcResult computes the result based on the current operator
+    private String calcResult() {
+        switch (operator) {
+            case "add":
+                return fixedNumber(prevNum + curNum);
+            case "divide":
+                return fixedNumber(prevNum / curNum);
+            case "substract":
+                return fixedNumber(prevNum - curNum);
+            case "multiply":
+                return fixedNumber(prevNum * curNum);
+            default:
+                return fixedNumber(curNum);
+        }
+    }
+
+    // fixedNumber makes sure the number is printed with a max of 5 digits after the
+    // decimal separator, and with no redundant zeros.
+    private String fixedNumber(double num) {
+        if (num == (long) num)
+            return String.format("%d", (long) num);
+        return String.format("%.5f", num).replaceAll("0*$", "");
+    }
+
+}
--- Mmn13Q2/CalculatorController.java
+++ Mmn13Q2/CalculatorController.java
@@ -4,174 +4,108 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
 
-// CalculatorController contains all the logic of the Calculator and the interaction with the GUI.
-// The calculator's logic is based on Apple's macOS basic calculator and follows the rules of the assignment.
-// It computes the result on every operation, so it doesn't help with operator precedence.
+// CalculatorController handles the interaction with the GUI.
 public class CalculatorController {
 
 	@FXML
 	private TextArea display;
-	private String operator = "";
-	private double prevNum = 0, curNum = 0;
-	private boolean opFlag = false; // will be true if the last action was an operation
+	private Calculator calculator;
 
-	// handleKeyPress routes key presses to the corresponding function
-	@FXML
-	private void handleKeyPress(KeyEvent key) {
-		String keyText = key.getText();
-		switch (keyText) {
-		case "0":
-		case "1":
-		case "2":
-		case "3":
-		case "4":
-		case "5":
-		case "6":
-		case "7":
-		case "8":
-		case "9":
-			executeNumberClick(keyText);
-			break;
-		case "+":
-			executeOperation("add");
-			break;
-		case "-":
-			executeOperation("substract");
-			break;
-		case "*":
-			executeOperation("multiply");
-			break;
-		case "/":
-			executeOperation("divide");
-			break;
-		case "=":
-			handleEquals();
-			break;
-		case ".":
-			handleComma();
-			break;
-		case "±":
-			handleChangeSign();
-			break;
-		default:
-			break;
-		}
+	// constructor
+	public CalculatorController() {
+		calculator = new Calculator();
 	}
 
-	// shandles the numerical buttons. It sets the text accordingly.
+	// handles the numerical buttons. sets the text accordingly.
 	@FXML
 	private void handleNum(Event event) {
 		Button btn = (Button) event.getSource();
-		String btnText = btn.getText();
-		executeNumberClick(btnText);
+		calculator.number(btn.getText());
+		displayResult();
 	}
 
-	// delete resets the calculator's memory and sets the calculator's text to an empty text
+	// resets the calculator's memory and sets the calculator's text to empty
 	@FXML
 	private void handleDelete() {
-		display.setText("");
-		prevNum = 0;
-		curNum = 0;
-		operator = "";
-		opFlag = false;
+		calculator.reset();
+		displayResult();
 	}
 
 	// handles clicks on +-/* buttons
 	@FXML
 	private void handleOperation(Event event) {
 		Button btn = (Button) event.getSource();
-		executeOperation(btn.getId());
+		calculator.operation(btn.getId());
+		displayResult();
 	}
 
-	// handleChangeSign simply changes the sign of the number that's displayed on screen.
-	// Note: this is the way OSX's calculator works, this is why I implemented it this way :)
+	// handles clicks on the ± change sign button
 	@FXML
 	private void handleChangeSign() {
-		double number = parseDouble(display.getText());
-		display.setText(fixedNumber(-number));
+		calculator.changeSign();
+		displayResult();
 	}
-	
+
 	// calculates and displays the result.
 	@FXML
 	private void handleEquals() {
-		curNum = parseDouble(display.getText());
-		display.setText(calcResult());
-		prevNum = parseDouble(display.getText());
-		operator = "";
-		opFlag = false;
+		calculator.equal();
+		displayResult();
 	}
-	
-	// handleComma adds a comma to the current text, unless there's already a comma
-	// (to prevent duplicates)
+
+	// handles comma
 	@FXML
 	private void handleComma() {
-		String curText = display.getText();
-		if (opFlag || curText.length() == 0) {
-			display.setText("0.");
-		} else if (!curText.contains(".")) {
-			display.setText(curText + ".");
-		}
-		opFlag = false;
-	}
-	
-	// executeOperation gets a string representing an operation (+, -, * or /). It
-	// calculates the last result, and only then it changes the current operator.
-	private void executeOperation(String op) {
-		if (opFlag) {
-			operator = op;
-		} else {
-			opFlag = true;
-			curNum = parseDouble(display.getText());
-			display.setText(calcResult());
-			prevNum = parseDouble(display.getText());
-			operator = op;
-		}
+		calculator.comma();
+		displayResult();
 	}
 
-	// gets the entered number and sets the display accordingly
-	private void executeNumberClick(String num) {
-		if (opFlag) {
-			display.setText(num);
-		} else {
-			display.setText(display.getText() + num);
-		}
-		opFlag = false;
-	}
-
-	// parseDouble is a wrapper around Double.parseDouble, but it also checks if the
-	// parsed text isn't empty and returns 0 if the parsing fails.
-	private double parseDouble(String text) {
-		if (text != null && text.length() > 0) {
-			try {
-				return Double.parseDouble(text);
-			} catch (NumberFormatException e) {
-				return 0;
-			}
-		}
-		return 0;
-	}
-	
-	// calcResult computes the result based on the current operator
-	private String calcResult() {
-		switch (operator) {
-			case "add":
-			return fixedNumber(prevNum + curNum);
-			case "divide":
-			return fixedNumber(prevNum / curNum);
-			case "substract":
-			return fixedNumber(prevNum - curNum);
-			case "multiply":
-			return fixedNumber(prevNum * curNum);
+	// handleKeyPress routes key presses to the corresponding function
+	@FXML
+	private void handleKeyPress(KeyEvent key) {
+		String keyText = key.getText();
+		switch (keyText) {
+			case "0":
+			case "1":
+			case "2":
+			case "3":
+			case "4":
+			case "5":
+			case "6":
+			case "7":
+			case "8":
+			case "9":
+				calculator.number(keyText);
+				break;
+			case "+":
+				calculator.operation("add");
+				break;
+			case "-":
+				calculator.operation("substract");
+				break;
+			case "*":
+				calculator.operation("multiply");
+				break;
+			case "/":
+				calculator.operation("divide");
+				break;
+			case "=":
+				calculator.equal();
+				break;
+			case ".":
+				calculator.comma();
+				break;
+			case "±":
+				calculator.changeSign();
+				break;
 			default:
-			return fixedNumber(curNum);
+				break;
 		}
+		displayResult();
 	}
 
-	// fixedNumber makes sure the number is printed with a max of 5 digits after the
-	// decimal separator, and with no redundant zeros.
-	private String fixedNumber(double num) {
-		if (num == (long) num)
-			return String.format("%d", (long) num);
-		return String.format("%.5f", num).replaceAll("0*$", "");
+	private void displayResult() {
+		display.setText(calculator.getCurrentResult());
 	}
+
 }
